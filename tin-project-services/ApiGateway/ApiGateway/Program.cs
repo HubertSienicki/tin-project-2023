cd79f8c8b5cf34af45719be2a924bbcaeaca6f74
HubertSienicki <businesssienicki@gmail.com>
2024-01-22 20:32:33 +0100

    add ocelot config

--- tin-project-services/ApiGateway/ApiGateway/Program.cs
+++ tin-project-services/ApiGateway/ApiGateway/Program.cs
@@ -1,3 +1,6 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddOcelot();
 
 var app = builder.Build();
 
@@ -19,6 +24,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseOcelot().Wait();
 
 app.MapControllers();
 
